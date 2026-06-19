fcec2ac5f9489b4f45719afeff659186de011af8
Shal1928 <Shal1928@yandex.ru>
2026-06-19 18:05:31 +0300

    v1.1.0+ [x] favicon in exe res [ ] install app

--- build.rs
+++ build.rs
@@ -2,6 +2,7 @@ fn main() {
     if cfg!(target_os = "windows") {
         winres::WindowsResource::new()
             .set_manifest_file("app.manifest")
+            .set_icon("favicon.ico")
             .compile()
             .unwrap();
     }
